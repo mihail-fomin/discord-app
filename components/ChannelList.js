a49ea814992f7dfd7b4c284f0f845c31b69e9c8a
misha133kv <mihail_fomin_95@mail.ru>
2023-03-24 22:34:27 +0500

    edited logic

--- components/ChannelList.js
+++ components/ChannelList.js
@@ -15,12 +15,16 @@ export const channels = [
 export default function ChannelList({ activeChannelID, setActiveChannelID }) {
 	const activeIndex = channels.findIndex(channel => channel.id == activeChannelID)
 
+	const textChannels = channels.filter(channel => channel.type == "text")
+	const voiceChannels = channels.filter(channel => channel.type == "voice")
+
 
 	const onTextChannelChange = (offset) => {
 		const result = channels.find((_channel, i) =>
 			offset == i
 		)
 		if (result) {
+
 			setActiveChannelID(result.id)
 		}
 	}
@@ -33,6 +37,11 @@ export default function ChannelList({ activeChannelID, setActiveChannelID }) {
 		}
 	}
 
+	function setOffset(type) {
+		return function setIndex(index) {
+
+		}
+	}
 	return <>
 		<section className='bg-zinc-700 basis-1/6'>
 			<Disclosure>
@@ -50,10 +59,9 @@ export default function ChannelList({ activeChannelID, setActiveChannelID }) {
 								icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 									<path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
 								</svg>}
-								items={channels}
+								items={textChannels}
 								activeIndex={activeIndex}
-								setActiveIndex={onChannelChange}
-
+								setActiveIndex={setOffset("text")}
 							/>
 						</Disclosure.Panel>
 					</>
@@ -75,8 +83,8 @@ export default function ChannelList({ activeChannelID, setActiveChannelID }) {
 									<path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
 								</svg>}
 								items={voiceChannels}
-								activeIndex={activeVoiceIndex}
-								setActiveIndex={onVoiceChannelChange}
+								activeIndex={activeIndex}
+								setActiveIndex={setOffset("voice")}
 
 							/>
 						</Disclosure.Panel>
