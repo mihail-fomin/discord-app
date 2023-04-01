2aa3fb40bb0a9003b33e66601231627bfc43a429
misha133kv <mihail_fomin_95@mail.ru>
2023-04-01 10:51:10 +0500

    added greetings screeen

--- components/Chat/MessageList.js
+++ components/Chat/MessageList.js
@@ -17,21 +17,26 @@ export default function MessageList({ contacts, history }) {
 	}
 
 	return <>
-		<div className="absolute bottom-5 left-10">
-			<ul className="">
-				{history.map(message => (
-					<li key={message.messageID}>
-						<div className="flex gap-4 my-4">
-							<img className="w-12 rounded-full" src={getAvatar(message.senderID)} alt="#" />
-							<div className="flex flex-col">
-								<div className="font-bold">{getName(message.senderID)}</div>
-								<div>{message.text}</div>
+		{(!history) ?
+			<div className="flex items-center justify-center h-full text-3xl">Welcome to the Game Center</div>
+			:
+			<div className="absolute bottom-5 left-10">
+				<ul>
+					{history.map(message => (
+						<li key={message.messageID}>
+							<div className="flex gap-4 my-4">
+								<img className="w-12 rounded-full" src={getAvatar(message.senderID)} alt="#" />
+								<div className="flex flex-col">
+									<div className="font-bold">{getName(message.senderID)}</div>
+									<div>{message.text}</div>
+								</div>
 							</div>
-						</div>
-					</li>
-				))}
-			</ul>
-		</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		}
+
 	</>
 }
 
